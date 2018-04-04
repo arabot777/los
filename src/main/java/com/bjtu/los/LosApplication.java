5e66c333e33ca4416a754fca9d3af4443cc7a197
Arabot <arabot777@163.com>
2018-04-04 19:57:44 +0800

    Initial commit

---
+++ src/main/java/com/bjtu/los/LosApplication.java
@@ -0,0 +1,18 @@
+package com.bjtu.los;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+//扫描相关注解
+@ComponentScan
+//从某个包去扫描mapper
+@MapperScan(basePackages = {"com.bjtu.los.mapper"})
+public class LosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LosApplication.class, args);
+	}
+}
