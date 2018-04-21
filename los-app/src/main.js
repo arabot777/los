ce24b8636e027a4e50784502046ee94f4f59df19
Arabot <arabot777@163.com>
2018-04-21 13:42:30 +0800

    页面展示

--- los-app/src/main.js
+++ los-app/src/main.js
@@ -1,15 +1,28 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+
 import Vue from 'vue'
 import App from './App'
+
 import router from './router'
+import store from './store'
+
+import lodash from 'lodash'
+
+import ElementUI from 'element-ui'
+import '@/assets/theme/index.css'
+
+import '@/assets/icon/iconfont.css'
+
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
+Vue.use(ElementUI)
+
+Object.defineProperty(Vue.prototype, '$_', { value: lodash })
+
 new Vue({
   el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
+  store,
+  template: '<App/>',
+  components: { App }
 })
