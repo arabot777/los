1ae945d4d44704d72412df9fb0450a1bf7679bce
imamoxicillin <13513626373@163.com>
2018-04-21 16:59:34 +0800

    查询所有场馆

--- src/main/java/com/bjtu/los/controller/StadiumController.java
+++ src/main/java/com/bjtu/los/controller/StadiumController.java
@@ -1,5 +1,23 @@
 package com.bjtu.los.controller;
 
+import com.bjtu.los.common.JsonData;
+import com.bjtu.los.server.StadiumService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/stadium")
 public class StadiumController {
+
+    @Autowired
+    private StadiumService stadiumService;
+
+    @GetMapping(value = "all")
+    public JsonData getAllStadium(){
+        return new JsonData(true).success(stadiumService.getAllStadium(),"成功查询所有场馆");
+    }
 }
--- src/main/java/com/bjtu/los/server/impl/StadiumServiceImpl.java
+++ src/main/java/com/bjtu/los/server/impl/StadiumServiceImpl.java
@@ -1,15 +1,21 @@
 package com.bjtu.los.server.impl;
 
+import com.bjtu.los.mapper.StadiumMapper;
 import com.bjtu.los.model.Stadium;
 import com.bjtu.los.server.StadiumService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
-public class StadiumServiceImpl extends StadiumService {
+@Service
+public class StadiumServiceImpl implements StadiumService {
 
+    @Autowired
+    private StadiumMapper stadiumMapper;
 
+    @Override
     public List<Stadium> getAllStadium(){
-        return new ArrayList<Stadium>();
+        return stadiumMapper.getAllStadium();
     }
 }
