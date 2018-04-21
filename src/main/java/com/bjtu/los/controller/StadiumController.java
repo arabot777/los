e91ebdca87be0f8f1715a53c1b39f826f5646b9e
imamoxicillin <13513626373@163.com>
2018-04-21 16:13:17 +0800

    添加场馆相关类

---
+++ src/main/java/com/bjtu/los/controller/StadiumController.java
@@ -0,0 +1,4 @@
+package com.bjtu.los.controller;
+
+public class StadiumController {
+}
---
+++ src/main/java/com/bjtu/los/mapper/StadiumMapper.java
@@ -0,0 +1,9 @@
+package com.bjtu.los.mapper;
+
+import com.bjtu.los.model.Stadium;
+
+import java.util.List;
+
+public interface StadiumMapper {
+    List<Stadium> getAllStadium();
+}
