f66a40d56955f7008c0760106aefb6d81a72f2d6
Arabot <arabot777@163.com>
2018-05-29 19:26:09 +0800

    vue前端

---
+++ los-app/src/main.js
@@ -0,0 +1,21 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import fastClick from 'fastClick'
+import 'styles/reset.css'
+import 'styles/border.css'
+import 'styles/iconfont.css'
+
+Vue.config.productionTip = false
+fastClick.attach(document.body)
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router, //router : router 
+  components: { App }, //{ App : App },
+  template: '<App/>'
+})
+
+//路由，根据网址的不同，返回不同的内容给用户
