1bf5ec9317875392d20a33c31f45dec0912a4e60
didi <arabotliushan_i@didichuxing.com>
2018-06-29 16:44:10 +0800

    登陆注册页面及对后端的请求

---
+++ los-app/src/pages/users/Login.vue
@@ -0,0 +1,23 @@
+<template>
+	<div>
+		<login-header></login-header>
+		<login-way></login-way>
+	</div>
+</template>
+
+<script>
+import LoginHeader from './components/LoginHeader'
+import LoginWay from './components/LoginWay'
+import axios from 'axios'
+export default {
+  name: 'Login',
+  components : {
+		LoginHeader,
+		LoginWay
+	}
+}
+</script>
+
+<style type="text/css" lang='stylus' scoped>
+
+</style>
---
+++ los-app/src/pages/users/components/RegisterHeader.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="header">
+    <span class="header-left iconfont">
+      &#xe624;
+    </span>
+    <div class="header-title">
+      注册
+    </div>
+    <router-link to='/login'>
+      <div class="header-right">
+        登录
+      </div>
+  </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+	 .header
+    position: relative;
+    line-height : $headerHeight;
+    background : $bgColor;
+    height: $headerHeight;
+    .header-left
+      left: 0;
+      padding-left: .1rem;
+      text-align: left;
+      font-family: iconfont,Tahoma;
+      font-size: .44rem;
+      font-weight: bold;
+      vertical-align: middle;
+    .header-title
+      margin: .2rem 1rem;
+      text-align: center;
+      line-height: .44rem;
+      color: #fff;
+      font-size: 0.43rem;
+      position: absolute;
+      top: 0;
+      width: 5.6rem;
+      height: .44rem;
+    .header-right
+      right: 0;
+      color: #fff;
+      padding-right: .3rem;
+      text-align: right;
+      font-size: .28rem;
+      position: absolute;
+      top: 0;
+      width: .6rem;
+      height: .44rem;
+      cursor: pointer;
+</style>
+
