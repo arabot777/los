ada731a61cfb027dd965dae99edadcb48544d49f
imamoxicillin <13513626373@163.com>
2018-07-01 00:22:35 +0800

    merge Simon_Wang to master.

--- src/main/java/com/bjtu/los/controller/StadiumController.java
+++ src/main/java/com/bjtu/los/controller/StadiumController.java
@@ -8,6 +8,7 @@ import com.bjtu.los.service.StadiumService;
 import com.google.common.collect.Lists;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.*;
