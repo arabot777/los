a66a03fab83e948e196056c9cc90471aa65a942b
imamoxicillin <13513626373@163.com>
2018-06-30 15:53:08 +0800

    fix bugs.

--- src/main/java/com/bjtu/los/service/impl/UserServiceImpl.java
+++ src/main/java/com/bjtu/los/service/impl/UserServiceImpl.java
@@ -4,7 +4,9 @@ import com.bjtu.los.mapper.UserMapper;
 import com.bjtu.los.model.User;
 import com.bjtu.los.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
